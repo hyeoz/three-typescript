592df53282155499e99cca0492b0434e2f35f855
hyeoz <jenny970203@gmail.com>
2023-06-16 16:22:14 +0900

    object 3d

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,45 +1,24 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Stats from 'three/examples/jsm/libs/stats.module'; // fps 확인 가능
+import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
-const scene2 = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
-  50, // 원근감
+  75, // 원근감
   window.innerWidth / window.innerHeight, // 카메라 비율
   0.1, // near plane
   1000, // far plane
 );
 camera.position.z = 2;
 
-const camera2 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
-const camera3 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
-const camera4 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
-camera2.position.y = 2;
-camera2.lookAt(new THREE.Vector3());
-camera3.position.x = 2;
-camera3.lookAt(new THREE.Vector3());
-camera4.position.z = 2;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight); // window 사이즈로 렌더러의 사이즈를 설정
+document.body.appendChild(renderer.domElement); // 1. canvas 를 직접 작성하지 않고 동적으로 추가
 
-const canvas1 = document.getElementById('c1') as HTMLCanvasElement; // 2. canvas 를 하드코딩 하여 추가
-const canvas2 = document.getElementById('c2') as HTMLCanvasElement; // 2. canvas 를 하드코딩 하여 추가
-const canvas3 = document.getElementById('c3') as HTMLCanvasElement; // 2. canvas 를 하드코딩 하여 추가
-const canvas4 = document.getElementById('c4') as HTMLCanvasElement; // 2. canvas 를 하드코딩 하여 추가
-
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight); // window 사이즈로 렌더러의 사이즈를 설정
-// document.body.appendChild(renderer.domElement); // 1. canvas 를 직접 작성하지 않고 동적으로 추가
-const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 });
-renderer1.setSize(200, 200);
-const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
-renderer2.setSize(200, 200);
-const renderer3 = new THREE.WebGLRenderer({ canvas: canvas3 });
-renderer3.setSize(200, 200);
-const renderer4 = new THREE.WebGLRenderer({ canvas: canvas4 });
-renderer4.setSize(200, 200);
-
-const controls = new OrbitControls(camera, renderer1.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
@@ -49,17 +28,12 @@ const material = new THREE.MeshBasicMaterial({
 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-const cube2 = new THREE.Mesh(geometry, material);
-cube2.scale.x = 0.5;
-cube2.scale.y = 0.5;
-cube2.scale.z = 0.5;
-scene2.add(cube2);
 
 window.addEventListener('resize', onWindowResize, false); // 이벤트 리스너를 통해 바뀌는 윈도우 사이즈를 바로 적용
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer1.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 }
 controls.addEventListener('change', render);
@@ -67,24 +41,42 @@ controls.addEventListener('change', render);
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
+const gui = new GUI();
+const cubeFolder = gui.addFolder('Cube');
+const cubeRotationFolder = cubeFolder.addFolder('CubeRotation');
+cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
+const cubePositionFolder = cubeFolder.addFolder('CubePosition');
+cubePositionFolder.add(cube.position, 'x', -10, 10);
+cubePositionFolder.add(cube.position, 'y', -10, 10);
+cubePositionFolder.add(cube.position, 'z', -10, 10);
+const cubeScaleFolder = cubeFolder.addFolder('CubeScale');
+cubeScaleFolder.add(cube.scale, 'x', 0, 10);
+cubeScaleFolder.add(cube.scale, 'y', 0, 10);
+cubeScaleFolder.add(cube.scale, 'z', 0, 10);
+
+cubeFolder.open();
+cubeRotationFolder.open();
+cubePositionFolder.open();
+cubeScaleFolder.open();
+// const cameraFolder = gui.addFolder('Camera');
+// cameraFolder.add(camera.position, 'z', 0, 20);
+// cameraFolder.open();
+
 function animate() {
   requestAnimationFrame(animate);
 
   // cube.rotation.x += 0.01;
   // cube.rotation.y += 0.01;
-  // cube2.rotation.x += 0.01;
-  // cube2.rotation.y += 0.01;
-  stats.update();
 
   render();
+
+  stats.update();
 }
 
 function render() {
-  // renderer.render(scene, camera);
-  renderer1.render(scene, camera);
-  renderer2.render(scene2, camera2);
-  renderer3.render(scene2, camera3);
-  renderer4.render(scene2, camera4);
+  renderer.render(scene, camera);
 }
 
 animate();
