87c86bea8c70e96aaa02f9488da7330ca600ae5a
hyeoz <jenny970203@gmail.com>
2023-06-19 10:39:26 +0900

    mesh basic material

--- src/client/client.ts
+++ src/client/client.ts
@@ -27,10 +27,24 @@ const planeGeometry = new THREE.PlaneGeometry();
 const torusKnotGeometry = new THREE.TorusKnotGeometry();
 
 const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,
+  // color: 0x00ff00,
+  // wireframe: true,
 });
 // const material = new THREE.MeshNormalMaterial();
+
+const texture = new THREE.TextureLoader().load('img/grid.png');
+material.map = texture;
+const envTexture = new THREE.CubeTextureLoader().load([
+  'img/px_50.png',
+  'img/nx_50.png',
+  'img/py_50.png',
+  'img/ny_50.png',
+  'img/pz_50.png',
+  'img/nz_50.png',
+]); // scene 을 둘러싸고 있는 env texture 는 cube texture 로 생성해줌
+envTexture.mapping = THREE.CubeReflectionMapping;
+// envTexture.mapping = THREE.CubeRefractionMapping;
+material.envMap = envTexture;
 
 const cube = new THREE.Mesh(boxGeometry, material);
 cube.position.x = 5;
@@ -69,13 +83,17 @@ const options = {
     BackSide: THREE.BackSide,
     DoubleSide: THREE.DoubleSide,
   },
+  combine: {
+    // material 과 envTexture 가 합쳐지는 옵션
+    MultiplyOperation: THREE.MultiplyOperation,
+    MixOperation: THREE.MixOperation,
+    AddOperation: THREE.AddOperation,
+  },
 };
 
 const gui = new GUI();
 const materialFolder = gui.addFolder('THREE.Material');
-materialFolder
-  .add(material, 'transparent')
-  .onChange(() => (material.needsUpdate = true));
+materialFolder.add(material, 'transparent').onChange(() => updateMaterial());
 materialFolder.add(material, 'opacity', 0, 1, 0.01);
 materialFolder.add(material, 'depthTest');
 materialFolder.add(material, 'depthWrite');
@@ -88,9 +106,26 @@ materialFolder
   .onChange(() => updateMaterial()); // 카메라 정면에서 보이는 side 결정
 materialFolder.open();
 
+const data = {
+  color: material.color.getHex(),
+};
+
+const meshBasicMaterialFolder = gui.addFolder('THREE.MeshBasicMaterial');
+meshBasicMaterialFolder.addColor(data, 'color').onChange(() => {
+  material.color.setHex(Number(data.color.toString().replace('#', '0x')));
+});
+meshBasicMaterialFolder.add(material, 'wireframe');
+meshBasicMaterialFolder.add(material, 'wireframeLinewidth', 0, 10);
+meshBasicMaterialFolder.add(material, 'reflectivity', 0, 1); // envTexture 가 mesh 에 비쳐지는 정도
+meshBasicMaterialFolder.add(material, 'refractionRatio', 0, 1); // envTexture 가 비쳐지는 굴절율
+meshBasicMaterialFolder
+  .add(material, 'combine', options.combine)
+  .onChange(() => updateMaterial());
+
 function updateMaterial() {
-  material.side = Number(material.side) as THREE.Side;
-  material.needsUpdate = true;
+  material.side = Number(material.side) as THREE.Side; // side
+  material.combine = Number(material.combine) as THREE.Combine; //combine
+  material.needsUpdate = true; // transparent, alphaTest
 }
 
 function animate() {
